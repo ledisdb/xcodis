c9ed3e6aa7bbf4a528f24fe2227f84a961f3f422
siddontang <siddontang@gmail.com>
2015-03-04 12:34:39 +0800

    update ledis white commands

--- proxy/router/ledis_helper.go
+++ proxy/router/ledis_helper.go
@@ -7,102 +7,124 @@ const (
 )
 
 func initWhiteListCommand() {
-	wc := whiteListCommand
+	regCmdType := func(tp string, cmds ...string) {
+		for _, cmd := range cmds {
+			whiteListCommand[cmd] = tp
+		}
+	}
 
-	wc["DECR"] = "KV"
-	wc["DECRby"] = "KV"
-	wc["DEL"] = "KV"
-	wc["EXISTS"] = "KV"
-	wc["GET"] = "KV"
-	wc["GETSET"] = "KV"
-	wc["INCR"] = "KV"
-	wc["INCRBY"] = "KV"
-	wc["MGET"] = "KV"
-	wc["MSET"] = "KV"
-	wc["SET"] = "KV"
-	wc["SETNX"] = "KV"
-	wc["SETEX"] = "KV"
-	wc["EXPIRE"] = "KV"
-	wc["EXPIREAT"] = "KV"
-	wc["TTL"] = "KV"
-	wc["PERSIST"] = "KV"
+	regCmdType("KV",
+		"DECR",
+		"DECRby",
+		"DEL",
+		"EXISTS",
+		"GET",
+		"GETSET",
+		"INCR",
+		"INCRBY",
+		"MGET",
+		"MSET",
+		"SET",
+		"SETNX",
+		"SETEX",
+		"EXPIRE",
+		"EXPIREAT",
+		"TTL",
+		"PERSIST",
+		"APPEND",
+		"GETRANGE",
+		"SETRANGE",
+		"STRLEN",
+		"BITCOUNT",
+		"BITPOS",
+		"GETBIT",
+		"SETBIT")
 
-	wc["HEDL"] = "HASH"
-	wc["HEXISTS"] = "HASH"
-	wc["HGET"] = "HASH"
-	wc["HGETALL"] = "HASH"
-	wc["HINCRBY"] = "HASH"
-	wc["HKEYS"] = "HASH"
-	wc["HLEN"] = "HASH"
-	wc["HMGET"] = "HASH"
-	wc["HMSET"] = "HASH"
-	wc["HSET"] = "HASH"
-	wc["HVALS"] = "HASH"
-	wc["HCLEAR"] = "HASH"
-	wc["HMCLEAR"] = "HASH"
-	wc["HEXPIRE"] = "HASH"
-	wc["HEXPIREAT"] = "HASH"
-	wc["HTTL"] = "HASH"
-	wc["HPERSIST"] = "HASH"
+	regCmdType("HASH",
+		"HEDL",
+		"HEXISTS",
+		"HGET",
+		"HGETALL",
+		"HINCRBY",
+		"HKEYS",
+		"HLEN",
+		"HMGET",
+		"HMSET",
+		"HSET",
+		"HVALS",
+		"HCLEAR",
+		"HMCLEAR",
+		"HEXPIRE",
+		"HEXPIREAT",
+		"HTTL",
+		"HPERSIST",
+		"HKEYEXISTS")
 
-	wc["LINDEX"] = "LIST"
-	wc["LLEN"] = "LIST"
-	wc["LPOP"] = "LIST"
-	wc["LRANGE"] = "LIST"
-	wc["LPUSH"] = "LIST"
-	wc["RPOP"] = "LIST"
-	wc["RPUSH"] = "LIST"
-	wc["LCLEAR"] = "LIST"
-	wc["LMCLEAR"] = "LIST"
-	wc["LEXPIRE"] = "LIST"
-	wc["LEXPIREAT"] = "LIST"
-	wc["LTTL"] = "LIST"
-	wc["LPERSIST"] = "LIST"
+	regCmdType("LIST",
+		"LINDEX",
+		"LLEN",
+		"LPOP",
+		"LRANGE",
+		"LPUSH",
+		"RPOP",
+		"RPUSH",
+		"LCLEAR",
+		"LMCLEAR",
+		"LEXPIRE",
+		"LEXPIREAT",
+		"LTTL",
+		"LPERSIST",
+		"LKEYEXISTS")
 
-	wc["SADD"] = "SET"
-	wc["SISMEMBER"] = "SET"
-	wc["SMEMBERS"] = "SET"
-	wc["SREM"] = "SET"
-	wc["SCLEAR"] = "SET"
-	wc["SMCLEAR"] = "SET"
-	wc["SEXPIRE"] = "SET"
-	wc["SEXPIREAT"] = "SET"
-	wc["STTL"] = "SET"
-	wc["SPERSIST"] = "SET"
+	regCmdType("SET",
+		"SADD",
+		"SISMEMBER",
+		"SMEMBERS",
+		"SREM",
+		"SCLEAR",
+		"SMCLEAR",
+		"SEXPIRE",
+		"SEXPIREAT",
+		"STTL",
+		"SPERSIST",
+		"SKEYEXISTS",
 
-	wc["ZADD"] = "ZSET"
-	wc["ZCARD"] = "ZSET"
-	wc["ZCOUNT"] = "ZSET"
-	wc["ZINCRBY"] = "ZSET"
-	wc["ZRANGE"] = "ZSET"
-	wc["ZRANGEBYSCORE"] = "ZSET"
-	wc["ZRANK"] = "ZSET"
-	wc["ZREM"] = "ZSET"
-	wc["ZREMRANGEBYRANK"] = "ZSET"
-	wc["ZREMRANGEBYSCORE"] = "ZSET"
-	wc["ZREVRANGE"] = "ZSET"
-	wc["ZREVRANK"] = "ZSET"
-	wc["ZREVRANGEBYSCORE"] = "ZSET"
-	wc["ZRANGEBYLEX"] = "ZSET"
-	wc["ZREMRANGEBYLEX"] = "ZSET"
-	wc["ZLEXCOUNT"] = "ZSET"
-	wc["ZCLEAR"] = "ZSET"
-	wc["ZMCLEAR"] = "ZSET"
-	wc["ZEXPIRE"] = "ZSET"
-	wc["ZEXPIREAT"] = "ZSET"
-	wc["ZTTL"] = "ZSET"
-	wc["ZPERSIST"] = "ZSET"
+		//below, all keys would have same hash (maybe same tag).
+		"SDIFF",
+		"SDIFFSTORE",
+		"SINTER",
+		"SINTERSTORE",
+		"SUNION",
+		"SUNIONSTORE")
 
-	//very dangerous, all keys would have same hash (maybe same tag).
-	wc["SDIFF"] = "SET"
-	wc["SDIFFSTORE"] = "SET"
-	wc["SINTER"] = "SET"
-	wc["SINTERSTORE"] = "SET"
-	wc["SUNION"] = "SET"
-	wc["SUNIONSTORE"] = "SET"
+	regCmdType("ZSET",
+		"ZADD",
+		"ZCARD",
+		"ZCOUNT",
+		"ZINCRBY",
+		"ZRANGE",
+		"ZRANGEBYSCORE",
+		"ZRANK",
+		"ZREM",
+		"ZREMRANGEBYRANK",
+		"ZREMRANGEBYSCORE",
+		"ZREVRANGE",
+		"ZREVRANK",
+		"ZREVRANGEBYSCORE",
+		"ZRANGEBYLEX",
+		"ZREMRANGEBYLEX",
+		"ZLEXCOUNT",
+		"ZCLEAR",
+		"ZMCLEAR",
+		"ZEXPIRE",
+		"ZEXPIREAT",
+		"ZTTL",
+		"ZPERSIST",
+		"ZKEYEXISTS",
 
-	wc["ZUNIONSTORE"] = "ZSET"
-	wc["ZINTERSTORE"] = "ZSET"
+		//below, all keys would have same hash (maybe same tag).
+		"ZUNIONSTORE",
+		"ZINTERSTORE")
 }
 
 func getOpGroup(op string) string {
